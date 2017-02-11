f7465b9333de0ac74e3e516273c9eee18e5e5576
Opher Vishnia <opherv@gmail.com>
2017-02-11 21:18:57 +0200

    Some progress

--- src/common/Car.js
+++ src/common/Car.js
@@ -1,25 +1,20 @@
 'use strict';
 
 const PhysicalObject = require('incheon').serialize.PhysicalObject;
-const MASS = 2;
+const MASS = 150;
 
 class Car extends PhysicalObject {
 
     constructor(id, gameEngine, position) {
         super(id, position);
         this.class = Car;
-        this.gameEngine = gameEngine;
+
     }
 
     onAddToWorld(gameEngine) {
 
-        // TODO: convert to rotation using aframe/system.js
-
-        // create the physics body
         this.gameEngine = gameEngine;
-        this.physicsObj = gameEngine.physicsEngine.addBox(2, 2, 4, MASS, 0);
-        this.physicsObj.position.set(this.position.x, this.position.y, this.position.z);
-        this.physicsObj.angularDamping = 0.1;
+        this.CANNON = gameEngine.physicsEngine.CANNON;
 
         let scene = gameEngine.renderer ? gameEngine.renderer.scene : null;
         if (scene) {
@@ -29,10 +24,95 @@ class Car extends PhysicalObject {
             let q = this.quaternion;
             el.setAttribute('position', `${p.x} ${p.y} ${p.z}`);
             el.object3D.quaternion.set(q.x, q.y, q.z, q.w);
-            el.setAttribute('material', 'color: red');
-            el.setAttribute('obj-model', 'obj: #car-obj');
+            // el.setAttribute('material', 'color: red');
+            // el.setAttribute('obj-model', 'obj: #car-obj');
             el.setAttribute('game-object-id', this.id);
         }
+
+        // TODO: convert to rotation using aframe/system.js
+
+        // let groundMaterial = new this.CANNON.Material("groundMaterial");
+        // let wheelMaterial = new this.CANNON.Material("wheelMaterial");
+        // let wheelGroundContactMaterial = new this.CANNON.ContactMaterial(wheelMaterial, groundMaterial, {
+        //     friction: 0.3,
+        //     restitution: 0,
+        //     contactEquationStiffness: 1000
+        // });
+        //
+        // // We must add the contact materials to the world
+        // this.gameEngine.physicsEngine.world.addContactMaterial(wheelGroundContactMaterial);
+
+        let chassisShape = new this.CANNON.Box(new this.CANNON.Vec3(2, 1, 0.5));
+        let chassisBody = this.physicsObj = new this.CANNON.Body({ mass: MASS });
+        chassisBody.addShape(chassisShape);
+        chassisBody.position.set(0, 0, 0);
+        // chassisBody.angularVelocity.set(0, 0, 0.5);
+        // demo.addVisual(chassisBody);
+
+        let options = {
+            radius: 0.5,
+            directionLocal: new this.CANNON.Vec3(0, 0, -1),
+            suspensionStiffness: 30,
+            suspensionRestLength: 0.3,
+            frictionSlip: 5,
+            dampingRelaxation: 2.3,
+            dampingCompression: 4.4,
+            maxSuspensionForce: 100000,
+            rollInfluence:  0.01,
+            axleLocal: new this.CANNON.Vec3(0, 1, 0),
+            chassisConnectionPointLocal: new this.CANNON.Vec3(1, 1, 0),
+            maxSuspensionTravel: 0.3,
+            customSlidingRotationalSpeed: -30,
+            useCustomSlidingRotationalSpeed: true
+        };
+
+        // Create the vehicle
+        this.vehicle = chassisBody.vehicle = new this.CANNON.RaycastVehicle({
+            chassisBody: chassisBody,
+        });
+
+        options.chassisConnectionPointLocal.set(1, 1, 0);
+        this.vehicle.addWheel(options);
+
+        options.chassisConnectionPointLocal.set(1, -1, 0);
+        this.vehicle.addWheel(options);
+
+        options.chassisConnectionPointLocal.set(-1, 1, 0);
+        this.vehicle.addWheel(options);
+
+        options.chassisConnectionPointLocal.set(-1, -1, 0);
+        this.vehicle.addWheel(options);
+
+        this.vehicle.addToWorld(this.gameEngine.physicsEngine.world);
+     
+        var wheelBodies = [];
+        for(let i=0; i < this.vehicle.wheelInfos.length; i++){
+            let wheel = this.vehicle.wheelInfos[i];
+            let cylinderShape = new this.CANNON.Cylinder(wheel.radius, wheel.radius, wheel.radius / 2, 20);
+            let wheelBody = new this.CANNON.Body({
+                mass: 0
+            });
+            wheelBody.type = this.CANNON.Body.KINEMATIC;
+            wheelBody.collisionFilterGroup = 0; // turn off collisions
+            let q = new this.CANNON.Quaternion();
+            q.setFromAxisAngle(new this.CANNON.Vec3(1, 0, 0), Math.PI / 2);
+            wheelBody.addShape(cylinderShape, new this.CANNON.Vec3(), q);
+            wheelBodies.push(wheelBody);
+            this.gameEngine.physicsEngine.world.addBody(wheelBody);
+        }
+
+        //todo remember to remove this handler!
+        this.gameEngine.physicsEngine.world.addEventListener('postStep', () => {
+            for (let i = 0; i < this.vehicle.wheelInfos.length; i++) {
+                this.vehicle.updateWheelTransform(i);
+                var t = this.vehicle.wheelInfos[i].worldTransform;
+                var wheelBody = wheelBodies[i];
+                wheelBody.position.copy(t.position);
+                wheelBody.quaternion.copy(t.quaternion);
+            }
+        });
+
+        this.physicsObj.position.set(this.position.x, this.position.y, this.position.z);
     }
 
     toString() {
@@ -40,7 +120,7 @@ class Car extends PhysicalObject {
     }
 
     destroy() {
-        this.gameEngine.physicsEngine.removeObject(this.physicsObj);
+        // this.gameEngine.physicsEngine.removeObject(this.vehicle);
     }
 
 }
--- src/common/SLGameEngine.js
+++ src/common/SLGameEngine.js
@@ -4,7 +4,10 @@ const GameEngine = require('incheon').GameEngine;
 const ThreeVector = require('incheon').serialize.ThreeVector;
 const Car = require('./Car');
 const Arena = require('./Arena');
-const TURN_IMPULSE = 0.04;
+
+const maxSteerVal = 0.5;
+const maxForce = 100;
+const brakeForce = 1000000;
 
 // todo check if this should be global
 let CANNON = null;
@@ -33,6 +36,7 @@ class SLGameEngine extends GameEngine {
         this.arena = new Arena(++this.world.idCount, this);
         this.arena.position.y = -4;
         this.addObjectToWorld(this.arena);
+        this.physicsEngine.world.defaultContactMaterial.friction = 0;
     }
 
     start() {
@@ -74,7 +78,7 @@ class SLGameEngine extends GameEngine {
         console.log(`adding car of player`, player);
 
         // create a fighter for this client
-        let position = new ThreeVector(0, 25, -5);
+        let position = new ThreeVector(0, 0, -10);
         let car = new Car(++this.world.idCount, this, position);
         car.playerId = player.playerId;
         this.addObjectToWorld(car);
@@ -95,23 +99,24 @@ class SLGameEngine extends GameEngine {
         let playerCar = this.world.getPlayerObject(playerId);
 
         if (playerCar) {
+            let vehicle = playerCar.vehicle;
+            vehicle.setBrake(0, 0);
+            vehicle.setBrake(0, 1);
+            vehicle.setBrake(0, 2);
+            vehicle.setBrake(0, 3);
+
             if (inputData.input === 'up') {
-                // todo probably bad perf
-                let newVec = playerCar.physicsObj.quaternion.vmult(new CANNON.Vec3(0, 0, 0.05));
-                // console.log(newVec);
-                playerCar.physicsObj.velocity.vadd(newVec, playerCar.physicsObj.velocity);
+                vehicle.applyEngineForce(-maxForce, 2);
+                vehicle.applyEngineForce(-maxForce, 3);
             } else if (inputData.input === 'right') {
-                let deltaAngularVelocity = playerCar.physicsObj.quaternion.vmult(new CANNON.Vec3(0, 1, 0));
-                deltaAngularVelocity.scale(-TURN_IMPULSE, deltaAngularVelocity);
-                playerCar.physicsObj.angularVelocity.vadd(deltaAngularVelocity, playerCar.physicsObj.angularVelocity);
+                vehicle.setSteeringValue(-maxSteerVal, 0);
+                vehicle.setSteeringValue(-maxSteerVal, 1);
             } else if (inputData.input === 'left') {
-                let deltaAngularVelocity = playerCar.physicsObj.quaternion.vmult(new CANNON.Vec3(0, 1, 0));
-                deltaAngularVelocity.scale(TURN_IMPULSE, deltaAngularVelocity);
-                playerCar.physicsObj.angularVelocity.vadd(deltaAngularVelocity, playerCar.physicsObj.angularVelocity);
+                vehicle.setSteeringValue(maxSteerVal, 0);
+                vehicle.setSteeringValue(maxSteerVal, 1);
             } else if (inputData.input === 'down') {
-                let newVec = playerCar.physicsObj.quaternion.vmult(new CANNON.Vec3(0, 0, -0.05));
-                // console.log(newVec);
-                playerCar.physicsObj.velocity.vadd(newVec, playerCar.physicsObj.velocity);
+                vehicle.applyEngineForce(maxForce, 2);
+                vehicle.applyEngineForce(maxForce, 3);
 
             }
 
